7a3be7f827230bf9be19faac399650ccf1d26d70
Priyanshi662 <priyanshiw567@gmail.com>
2023-09-16 18:12:05 +0530

    Learning typescript

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+// const n="Mario";
+// console.log(n);
+// const inputs=document.querySelectorAll('input');
+// console.log(inputs);
+// inputs.forEach(input=>{
+//     console.log(input);
+// });
---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+const n="Mario";
+console.log(n);
+const inputs=document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach(input=>{
+    console.log(input);
+});
