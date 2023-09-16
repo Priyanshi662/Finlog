1160866bb2033b2717bffab4a9ba0fd591534a4d
Priyanshi662 <priyanshiw567@gmail.com>
2023-09-16 18:49:33 +0530

    3rd

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,4 @@
+// 1st
 // const n="Mario";
 // console.log(n);
 // const inputs=document.querySelectorAll('input');
@@ -5,8 +6,7 @@
 // inputs.forEach(input=>{
 //     console.log(input);
 // });
-// 2nd session
-// types basics
+// 2nd -> types basics
 // let na="mario";
 // na=2; -> gives error because we cant assign value of different type
 // can specify type of values in function parameters so that correct type is passed
@@ -14,3 +14,28 @@ var circumf = function (diameter) {
     return 2 * diameter * Math.PI;
 };
 console.log(circumf(34));
+// 3rd -> arrays and objects
+var arr = [23, 34, 56];
+arr.push(45);
+// arr.push("pi"); -> will not work because of different type
+// to create arrays and objects which will have their type determined later use:
+var arr1 = [];
+var obj = {};
+// mixed arrays
+var mixarr = [23, "pri", "yan", "shi", 45];
+mixarr.push(5);
+// mixarr.push(true); -> will not work because arr is of type string|int
+// array's type is determined at the declaration
+// objects :
+var student = {
+    name: "Adam",
+    age: 20,
+    course: "Physics"
+};
+// student.class="3-A"; -> cannot add any other attribute once declared
+student = {
+    name: "Adam",
+    age: 20,
+    course: "Physics",
+    // class:"3-A" -> cannot add the attribute to the object
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,4 @@
+// 1st
 // const n="Mario";
 // console.log(n);
 // const inputs=document.querySelectorAll('input');
@@ -7,8 +8,7 @@
 //     console.log(input);
 // });
 
-// 2nd session
-// types basics
+// 2nd -> types basics
 // let na="mario";
 // na=2; -> gives error because we cant assign value of different type
 // can specify type of values in function parameters so that correct type is passed
@@ -17,3 +17,29 @@ const circumf=(diameter:number)=>
     return 2*diameter*Math.PI;
 }
 console.log(circumf(34));
+
+// 3rd -> arrays and objects
+let arr=[23,34,56];
+arr.push(45);
+// arr.push("pi"); -> will not work because of different type
+// to create arrays and objects which will have their type determined later use:
+let arr1=[];
+let obj={};
+// mixed arrays
+let mixarr=[23,"pri","yan","shi",45];
+mixarr.push(5);
+// mixarr.push(true); -> will not work because arr is of type string|int
+// array's type is determined at the declaration
+// objects :
+let student={
+    name:"Adam",
+    age:20,
+    course:"Physics"
+};
+// student.class="3-A"; -> cannot add any other attribute once declared
+student={
+    name:"Adam",
+    age:20,
+    course:"Physics",
+    // class:"3-A" -> cannot add the attribute to the object
+}
